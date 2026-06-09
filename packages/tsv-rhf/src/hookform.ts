3ea3be7bb8a4e9140482ecba2eb6103f2a15bfad
Marco Girometta <girometta.marco@gmail.com>
2026-06-09 23:31:49 +0200

    Update react-hook-form resolver

--- packages/tsv-rhf/src/hookform.ts
+++ packages/tsv-rhf/src/hookform.ts
@@ -4,97 +4,13 @@ import type {
   Schema,
   SchemaValues
 } from '@gmcode/tsv-core'
+import { toNestErrors, validateFieldsNatively } from '@hookform/resolvers'
 import {
-  type Field,
-  type FieldError,
-  type FieldErrors,
-  type FieldValues,
-  type InternalFieldName,
-  type Ref,
   type Resolver,
   type ResolverError,
   type ResolverOptions,
-  type ResolverSuccess,
-  get,
-  set
+  type ResolverSuccess
 } from 'react-hook-form'
-
-const setCustomValidity = (
-  ref: Ref,
-  fieldPath: string,
-  errors: FieldErrors
-) => {
-  if (ref && 'reportValidity' in ref) {
-    const error = get(errors, fieldPath) as FieldError | undefined
-    ref.setCustomValidity((error && error.message) || '')
-
-    ref.reportValidity()
-  }
-}
-
-// Native validation (web only)
-const validateFieldsNatively = <TFieldValues extends FieldValues>(
-  errors: FieldErrors,
-  options: ResolverOptions<TFieldValues>
-): void => {
-  for (const fieldPath in options.fields) {
-    const field = options.fields[fieldPath]
-    if (field && field.ref && 'reportValidity' in field.ref) {
-      setCustomValidity(field.ref, fieldPath, errors)
-    } else if (field && field.refs) {
-      field.refs.forEach((ref: HTMLInputElement) =>
-        setCustomValidity(ref, fieldPath, errors)
-      )
-    }
-  }
-}
-
-const toNestErrors = <TFieldValues extends FieldValues>(
-  errors: FieldErrors,
-  options: ResolverOptions<TFieldValues>
-): FieldErrors<TFieldValues> => {
-  if (options.shouldUseNativeValidation) {
-    validateFieldsNatively(errors, options)
-  }
-
-  const fieldErrors = {} as FieldErrors<TFieldValues>
-  for (const path in errors) {
-    const field = get(options.fields, path) as Field['_f'] | undefined
-    const error = Object.assign(errors[path] || {}, {
-      ref: field && field.ref
-    })
-
-    if (isNameInFieldArray(options.names || Object.keys(errors), path)) {
-      const fieldArrayErrors = Object.assign({}, get(fieldErrors, path))
-
-      set(fieldArrayErrors, 'root', error)
-      set(fieldErrors, path, fieldArrayErrors)
-    } else {
-      set(fieldErrors, path, error)
-    }
-  }
-
-  return fieldErrors
-}
-
-const isNameInFieldArray = (
-  names: InternalFieldName[],
-  name: InternalFieldName
-) => {
-  const path = escapeBrackets(name)
-  return names.some((n) => escapeBrackets(n).match(`^${path}\\.\\d+`))
-}
-
-/**
- * Escapes special characters in a string to be used in a regex pattern.
- * it removes the brackets from the string to match the `set` method.
- *
- * @param input - The input string to escape
- * @returns The escaped string
- */
-function escapeBrackets(input: string): string {
-  return input.replace(/\]|\[/g, '')
-}
 
 /**
  * Custom hook to generate a validation resolver for React Hook Form.
@@ -110,6 +26,10 @@ export function useTsvResolver<
   // Resolver
   return (values: Input, context: unknown, options: ResolverOptions<Input>) => {
     const { errors, sanitized, success } = schema.validate(values)
+
+    if (options.shouldUseNativeValidation) {
+      validateFieldsNatively({}, options)
+    }
 
     if (success) {
       return {
