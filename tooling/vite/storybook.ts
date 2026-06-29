507204a28c31d2c476e501a09f74538cc333f826
Marco Girometta <girometta.marco@gmail.com>
2026-06-29 11:05:40 +0200

    Update storybook config

--- tooling/vite/storybook.ts
+++ tooling/vite/storybook.ts
@@ -3,21 +3,18 @@ import { mergeConfig } from 'vite-plus'
 import { playwright } from 'vite-plus/test/browser-playwright'
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin'
 
-/** Vitest or Storybook environment */
-const testing = process.env.VITEST || process.argv[1]?.includes('storybook')
-
 export default mergeConfig(defaultConfig, {
   test: {
     projects: [
       {
         extends: true,
-        plugins: [testing ? storybookTest() : null],
+        plugins: [storybookTest()],
         test: {
           browser: {
             enabled: true,
             headless: true,
             instances: [{ browser: 'chromium' }],
-            provider: playwright({})
+            provider: playwright()
           },
           name: 'storybook'
         }
